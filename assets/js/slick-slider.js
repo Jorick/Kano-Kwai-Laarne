4004de20dbde8bd4e8eaee8e3173ad63a4b668f5
Jorick <roels.jorick@gmail.com>
2021-02-06 16:32:47 +0100

    adding content

---
+++ assets/js/slick-slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.foto-gallery').slick({
+    infinite: true,
+    autoPlay: true,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1
+    });
+});
