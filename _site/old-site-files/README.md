e611066fd895748fcb9fb650470e56b355e53d7b
Jorick <roels.jorick@gmail.com>
2021-02-06 14:12:00 +0100

    first runable version

---
+++ _site/old-site-files/README.md
@@ -0,0 +1,4 @@
+site-judo2
+==========
+
+Dev version of the website for Kano Kwai Laarne.
---
+++ _site/old-site-files/js/sportdienstscript.js
@@ -0,0 +1,5 @@
+var string1 = "@";
+var string2 = "laarne.be";
+var string3 = "sportdienst";
+var string4 = string3 + string1 + string2;
+document.write("<a href=" + "mail" + "to:" + string3 + string1 + string2 + ">" + string4 + "</a>");
