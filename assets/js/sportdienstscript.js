c2ad43562d12c5f7d7e39787a44e46ecb48a33ab
Jorick <roels.jorick@gmail.com>
2021-02-14 14:03:41 +0100

    contact buttons and small fixes

--- assets/js/sportdienstscript.js
+++ assets/js/sportdienstscript.js
@@ -1,5 +1,4 @@
 var string1 = "@";
 var string2 = "laarne.be";
 var string3 = "sportdienst";
-var string4 = string3 + string1 + string2;
-document.write("<a href=" + "mail" + "to:" + string3 + string1 + string2 + ">" + string4 + "</a>");
+document.write("<a class=\"btn btn-secondary\" href=" + "mail" + "to:" + string3 + string1 + string2 + "><i class=\"bi-envelope\"></i> - Sportdienst</a>");
